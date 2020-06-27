94e860d5967484e4a40228126d415f7be0a4638e
KalliFurkan <kallifurkan@gmail.com>
2020-06-27 17:16:41 +0300

    v2-27-06-2020
    First UV MAPPING TEXTURE

---
+++ Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class ItemPickUp : Interactable {
+    public Item item;
+    public override void Interact() {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp() {
+
+        Debug.Log("picking up item" + item.name);
+        Inventory.instance.Add(item);
+        // add to inventory
+        Destroy(gameObject);
+    }
+}
